ce0e879deb04757b43424351874ef3e2c6b6b091
jclasley <jon.lasley@gmail.com>
2024-08-19 19:14:43 -0400

    license

--- sticky-scroll-mode.el
+++ sticky-scroll-mode.el
@@ -2,6 +2,18 @@
 ;;
 ;; Copyright (C) 2024 Jon Lasley
 ;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
 ;; Author: Jon Lasley <jon.lasley+sticky@gmail.com>
 ;; Maintainer: Jon Lasley <jon.lasley+sticky@gmail.com>
 ;; Created: August 09, 2024
